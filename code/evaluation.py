daa6992980faf0495f659bf5cc2e9d053cd95783
rohandhupar1996 <rohandhupar18@gmail.com>
2024-05-19 12:37:59 +0530

    evaluation f1 score added

--- code/evaluation.py
+++ code/evaluation.py
@@ -34,9 +34,23 @@ class EmotionEvaluation:
             results[emotion] = {'Precision': precision, 'Recall': recall, 'F1 Score': f1}
         return results
 
+    def calculate_average_f1(self):
+        f1_scores = [self.calculate_precision_recall_f1()[emotion]['F1 Score'] for emotion in self.emotions]
+        macro_f1 = sum(f1_scores) / len(f1_scores)  # Macro-average F1
+        # Micro-average F1
+        total_TP = sum([self.confusion_matrices[emotion]['TP'] for emotion in self.emotions])
+        total_FP = sum([self.confusion_matrices[emotion]['FP'] for emotion in self.emotions])
+        total_FN = sum([self.confusion_matrices[emotion]['FN'] for emotion in self.emotions])
+        total_precision = total_TP / (total_TP + total_FP)
+        total_recall = total_TP / (total_TP + total_FN)
+        micro_f1 = 2 * (total_precision * total_recall) / (total_precision + total_recall)
+        return {'Macro F1': macro_f1, 'Micro F1': micro_f1}
+
 # # Example usage:
 # emotions = ['Anger', 'Anticipation', 'Disgust', 'Fear', 'Joy', 'Sadness', 'Surprise', 'Trust']
 # evaluation = EmotionEvaluation(y_true, y_pred, emotions)  # y_true and y_pred need to be lists of dictionaries
 # evaluation.compute_metrics()
 # final_scores = evaluation.calculate_precision_recall_f1()
+# average_f1_scores = evaluation.calculate_average_f1()
 # print(final_scores)
+# print("Average F1 Scores:", average_f1_scores)
--- code/main_nb.py
+++ code/main_nb.py
@@ -1,59 +1,9 @@
-from collections import Counter
-import math
+#Import our custom classes for Preprocess, Naive Bayes, and Evaluation
 
-class MultiLabelNaiveBayes:
-    def __init__(self):
-        self.models = {}
+from preprocess import TextDataProcessor
+from modified_nb import MultiLabelNaiveBayes
+from evaluation import EmotionEvaluation
 
-    def train(self, X, Y):
-        # Train a separate Naive Bayes model for each label
-        for column in Y.columns:
-            self.models[column] = self.train_single_label(X, Y[column])
-
-    def train_single_label(self, X, y):
-        # Frequency of each class
-        class_count = Counter(y)
-        doc_count = len(y)
-        
-        # Calculate prior probabilities
-        prior_probabilities = {cls: math.log(class_count[cls] / doc_count) for cls in class_count}
-        
-        # Feature counting per class
-        feature_counts = {}
-        for cls in class_count:
-            feature_counts[cls] = Counter()
-        
-        # Count features in each class
-        for text, label in zip(X, y):
-            feature_counts[label].update(text.split())  # Assuming the simplest case where features are words
-        
-        # Calculate feature probabilities
-        feature_probabilities = {}
-        for cls in class_count:
-            total_features = sum(feature_counts[cls].values()) + len(feature_counts[cls])  # Laplace smoothing
-            feature_probabilities[cls] = {word: math.log((feature_counts[cls].get(word, 0) + 1) / total_features) 
-                                          for word in feature_counts[cls]}
-        
-        return {'prior': prior_probabilities, 'likelihood': feature_probabilities}
-
-    def predict(self, X):
-        results = []
-        for text in X:
-            result = {}
-            for emotion, model in self.models.items():
-                log_prob = {cls: model['prior'][cls] for cls in model['prior']}  # Start with the prior probabilities
-                words = text.split()  # Split text into words
-                for word in words:
-                    for cls in model['likelihood']:
-                        log_prob[cls] += model['likelihood'][cls].get(word, 0)  # Add the log probability of each word
-                
-                # Find the class with the highest log probability
-                predicted_class = max(log_prob, key=log_prob.get)
-                result[emotion] = predicted_class
-            results.append(result)
-        return results
-
-# # Example usage
-# nb_model = MultiLabelNaiveBayes()
-# nb_model.train(X_train, Y_train)  # X_train and Y_train need to be defined
-# predictions = nb_model.predict(X_test)  # X_test needs to be defined
+train = '../data/ssec/train.csv'
+test = '../data/ssec/test.csv' 
+dev = '../data/val/val.csv'
